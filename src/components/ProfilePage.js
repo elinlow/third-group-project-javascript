2e1356cc97419726a34a0a6faee3ec810c2c16b7
elliot9802 <elliot@segerlind.se>
2022-01-14 15:55:48 +0100

    Created ProfilePage component

---
+++ src/components/ProfilePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import SharePictures from './SharePicture'
+import UserBio from './UserBio'
+
+function ProfilePage() {
+    return (
+        <div>
+            <SharePictures></SharePictures>
+            <UserBio></UserBio>
+        </div>
+    )
+}
+
+export default ProfilePage
