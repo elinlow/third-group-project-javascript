dda7d03b7c4f60bbb84d9844521972665d2d12b1
elinlow <elin.andersson.lowenadler@gmail.com>
2022-01-14 13:08:32 +0100

    imported reusable button to likepicture component

--- src/components/LikePicture.js
+++ src/components/LikePicture.js
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
+import ReusableButton from './ReusableButton';
 
 const LikePicture = () => {
   const [count, setCount] = useState(0);
 
   return (
     <div>
-      <p>You have {count} likes</p>
-      <button onClick={() => setCount(count + 1)}>
+      <p>This picture has {count} likes</p>
+      <button className="button" onClick={() => setCount(count + 1)}>
         Like
       </button>
     </div>
   );
 }
+
+export default LikePicture;
