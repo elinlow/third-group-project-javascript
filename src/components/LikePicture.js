d71fed9238b6393b7f69df716d08fb8affa41a36
elinlow <elin.andersson.lowenadler@gmail.com>
2022-01-15 22:19:10 +0100

    made changes to show image

--- src/components/LikePicture.js
+++ src/components/LikePicture.js
@@ -2,16 +2,16 @@ import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart, faComment } from "@fortawesome/free-solid-svg-icons";
 import '../LikePicture.css';
-import cuteDog from './assets/dog.png';
+import cuteDog from '../assets/dog.png'
 
 const LikePicture = () => {
   const [count, setCount] = useState(0);
 
   return (
     <div>
-      <img src="cuteDog" alt="Elin's picture" />
+      <img src={cuteDog} alt="Elin's picture" className="dog" />
       <p>This picture has {count} likes</p>
-      <button className="likeButton" onClick={() => setCount(count + 1)}>
+      <button className="like-button" onClick={() => setCount(count + 1)}>
         <FontAwesomeIcon icon={faHeart} />
       </button>
     </div>
